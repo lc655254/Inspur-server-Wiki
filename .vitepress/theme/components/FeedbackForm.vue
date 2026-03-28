11c984c45240e49f6362fbd19bf42a7e273abc06
浪潮 <145839063+lc655254@users.noreply.github.com>
2026-03-28 15:59:51 +0800

    Update FeedbackForm.vue

--- .vitepress/theme/components/FeedbackForm.vue
+++ .vitepress/theme/components/FeedbackForm.vue
@@ -87,7 +87,7 @@ export default {
   data() {
     return {
       // 👇 这是你自己电脑的后端地址（本地运行）
-      baseURL: "http://api.inspurs.work",
+      baseURL: "https://api.inspurs.work",
       
       form: {
         playerName: '',
